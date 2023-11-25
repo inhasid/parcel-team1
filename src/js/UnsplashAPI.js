42c0119414e0c813da749d94d44dc98f75a783f6
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-11-25 12:55:17 +0200

    found

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -2,19 +2,40 @@ import axios from 'axios';
 
 export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com/search/photos';
-    #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
-    
+  #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+  #query = '';
+  #params = new URLSearchParams({
+    per_page: 12,
+    orientation: 'portrait',
+    client_id: this.#API_KEY,
+  });
+
   async getPopularImages(page) {
-    const url = `${
-      this.#BASE_URL
-    }?query=popular&page=${page}&per_page=12&orientation=portrait&client_id=${
-      this.#API_KEY
-    }`;
-      try {            
-      const {data}  = await axios(url);
+    // const url = `${
+    //   this.#BASE_URL
+    // }?query=popular&page=${page}&per_page=12&orientation=portrait&client_id=${
+    //   this.#API_KEY
+    // }`;
+    const url = `${this.#BASE_URL}?query=popular&page=${page}&${this.#params}`;
+    try {
+      const { data } = await axios(url);
       return data;
     } catch (error) {
       console.log(error);
     }
+  }
+  async getImagesByQuery(page) {
+    const url = `${this.#BASE_URL}?query=${
+      this.#query
+    }&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`;
+    try {
+      const { data } = await axios(url);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  set query(newQuery) {
+    this.#query = newQuery;
   }
 }
